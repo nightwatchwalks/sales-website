27bd45fba1aa89d75f72d67484132b674fe26080
Yigit Duman <yigitq@gmail.com>
2023-08-29 15:54:21 +0300

    update config for sepolia

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,9 +2,9 @@ import { Hex } from "viem";
 
 export const config = {
   addresses: {
-    nightWatch: "0x3389D3b8eD9F863e8F55Bdf3A5efc5e01c5DbCC2" as Hex,
-    vendor: "0xAEf87124dC2784BC4cE21dBc852F6FD293df8E6b" as Hex,
-    vault: "0x93cb7A0238B25679b6389719AfD6D91FC4c855ac" as Hex,
+    nightWatch: "0x1f508DcB7C3eD18c967df3b20A1567e2429825e9" as Hex,
+    vendor: "0x14424BdB8426e7fb9F77557c30C8edBA746454f2" as Hex,
+    vault: "0x9C28c036406F87fcC236898b6ED90CBe0BEa91C5" as Hex,
   },
   apis: {
     walletConnect: "3589d86efd1ff44bc46d3659de3e0c90",
@@ -15,5 +15,5 @@ export const config = {
   rpcUrl: "https://sepolia.infura.io/v3/b3bb49d22cc24fd08a1fe28db11768a1",
   pricePerToken: 0.03,
   chainId: 11155111,
-  minLookupBlock: 4184266,
+  minLookupBlock: 4184381,
 };
