b3fc95e3e21696cd7a4bcd28b2a357943b8bea8c
Yigit Duman <yigitq@gmail.com>
2023-08-31 03:24:56 +0300

    update sale website

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,9 +2,9 @@ import { Hex } from "viem";
 
 export const config = {
   addresses: {
-    nightWatch: "0x1f508DcB7C3eD18c967df3b20A1567e2429825e9" as Hex,
-    vendor: "0x14424BdB8426e7fb9F77557c30C8edBA746454f2" as Hex,
-    vault: "0x9C28c036406F87fcC236898b6ED90CBe0BEa91C5" as Hex,
+    nightWatch: "0xEf0b00d38eED201733F2AA6328e5f56891A57BB2" as Hex,
+    vendor: "0xD1C833b46266CcF6060e90B69161FCEe9C66E826" as Hex,
+    vault: "0x96e4fe738f991AfAeF7B1e55eaB5B528D3C73d60" as Hex,
   },
   apis: {
     walletConnect: "3589d86efd1ff44bc46d3659de3e0c90",
@@ -12,8 +12,8 @@ export const config = {
   metadataServerRoot: "https://metadata-server.impossibletrios.art",
   generatedGifsRoot:
     "https://night-watch-walks.s3.us-east-1.amazonaws.com/generated-gifs",
-  rpcUrl: "https://sepolia.infura.io/v3/b3bb49d22cc24fd08a1fe28db11768a1",
+  rpcUrl: "https://mainnet.infura.io/v3/b3bb49d22cc24fd08a1fe28db11768a1",
   pricePerToken: 0.03,
-  chainId: 11155111,
-  minLookupBlock: 4184381,
+  chainId: 1,
+  minLookupBlock: 18024804,
 };
